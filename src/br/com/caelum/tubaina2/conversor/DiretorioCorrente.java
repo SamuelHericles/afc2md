2699d43ab31599bb6f0204f847a6d4a2b47e2c9c
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-02-11 17:34:06 -0200

    classe que trata o diretorio dos arquivos .afc

---
+++ src/br/com/caelum/tubaina2/conversor/DiretorioCorrente.java
@@ -0,0 +1,12 @@
+package br.com.caelum.tubaina2.conversor;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DiretorioCorrente {
+
+	private Path diretorio = Paths.get("").toAbsolutePath();
+
+
+
+}
