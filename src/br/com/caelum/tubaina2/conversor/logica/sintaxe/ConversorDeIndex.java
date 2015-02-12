0bf4df03a558c2e5a43d87514f85bd400605e1fa
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-02-12 09:20:57 -0200

    isolando logica de conversao do [index]

---
+++ src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeIndex.java
@@ -0,0 +1,13 @@
+package br.com.caelum.tubaina2.conversor.logica.sintaxe;
+
+public class ConversorDeIndex implements ConversorDeSintaxe {
+
+	private static final String REGEX_INDEX = "\\[index .*\\]";
+	private static final String REPLACEMENT_INDEX = "";
+	
+	@Override
+	public String converte(String sintaxe) {
+		return sintaxe.replaceAll(REGEX_INDEX, REPLACEMENT_INDEX);
+	}
+
+}
