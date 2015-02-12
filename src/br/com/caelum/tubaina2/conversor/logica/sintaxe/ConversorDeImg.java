0efb529dba8a4fcc70e572a3385623c42df86802
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-02-12 09:19:11 -0200

    isolando logica de conversao do [img]

---
+++ src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeImg.java
@@ -0,0 +1,17 @@
+package br.com.caelum.tubaina2.conversor.logica.sintaxe;
+
+public class ConversorDeImg implements ConversorDeSintaxe {
+
+	private static final String REGEX_IMG = "\\[img (\\S+)[ ]?(w=[0-9]{1,3})?([ ]\"(.*)\")?\\]";
+	private static final String REGEX_EMPTY_WIDTH = "[ ]\\{\\}(\\]\\(.*\\))";
+	private static final String REPLACEMENT_IMG = "![$4 {$2}]($1)";
+	private static final String REPLACEMENT_EMPTY_WIDTH = "$1";
+	
+	@Override
+	public String converte(String sintaxe) {
+		return sintaxe
+				.replaceAll(REGEX_IMG, REPLACEMENT_IMG)
+				.replaceAll(REGEX_EMPTY_WIDTH, REPLACEMENT_EMPTY_WIDTH);
+	}
+
+}
