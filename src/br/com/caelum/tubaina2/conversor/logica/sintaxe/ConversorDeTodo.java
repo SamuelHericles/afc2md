e1a2ab29e723c5364c4c6745cf307b461b61ded3
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-03-30 15:08:19 -0300

    adicionando conversor de [todo]

---
+++ src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeTodo.java
@@ -0,0 +1,13 @@
+package br.com.caelum.tubaina2.conversor.logica.sintaxe;
+
+public class ConversorDeTodo implements ConversorDeSintaxe {
+
+	private static final String REGEX_TODO = "\\[(?i)TODO(.*)\\]";
+	private static final String REPLACEMENT_TODO = "<!-- $1 -->";
+
+	@Override
+	public String converte(String sintaxe) {
+		return sintaxe.replaceAll(REGEX_TODO, REPLACEMENT_TODO);
+	}
+	
+}
