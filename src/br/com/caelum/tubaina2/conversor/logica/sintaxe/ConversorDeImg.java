10c99c9921a26e04585ce04235932ee6fff86189
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-03-30 15:07:23 -0300

    ajustes no conversor de [img]

--- src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeImg.java
+++ src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeImg.java
@@ -2,7 +2,7 @@ package br.com.caelum.tubaina2.conversor.logica.sintaxe;
 
 public class ConversorDeImg implements ConversorDeSintaxe {
 
-	private static final String REGEX_IMG = "\\[img (\\S+)[ ]?(w=[0-9]{1,3})?([ ]\"(.*)\")?\\]";
+	private static final String REGEX_IMG = "\\[img (\\S+)[ ]?(w=[0-9]{1,3}%?)?([ ]\"(.*)\")?\\]";
 	private static final String REGEX_EMPTY_WIDTH = "[ ]\\{\\}(\\]\\(.*\\))";
 	private static final String REPLACEMENT_IMG = "![$4 {$2}]($1)";
 	private static final String REPLACEMENT_EMPTY_WIDTH = "$1";
