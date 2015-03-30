13be6e9c466c3fd15c5607ea7a19c799d55347a0
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-03-30 15:07:00 -0300

    ajustes no conversor de [code]

--- src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeCode.java
+++ src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeCode.java
@@ -4,14 +4,17 @@ public class ConversorDeCode implements ConversorDeSintaxe {
 
 	private static final String REGEX_ABERTURA_CODE = "\\[code(.*)\\]";
 	private static final String REGEX_FECHAMENTO_CODE = "\\[/code\\]";
+	private static final String REGEX_HIGHLIGHT = "h=(.*)";
 	private static final String REPLACEMENT_ABERTURA_CODE = "```$1";
 	private static final String REPLACEMENT_FECHAMENTO_CODE = "```";
+	private static final String REPLACEMENT_HIGHLIGHT = "";
 
 	@Override
 	public String converte(String sintaxe) {
 		return sintaxe
 				.replaceAll(REGEX_ABERTURA_CODE, REPLACEMENT_ABERTURA_CODE)
-				.replaceAll(REGEX_FECHAMENTO_CODE, REPLACEMENT_FECHAMENTO_CODE);
+				.replaceAll(REGEX_FECHAMENTO_CODE, REPLACEMENT_FECHAMENTO_CODE)
+				.replaceAll(REGEX_HIGHLIGHT, REPLACEMENT_HIGHLIGHT);
 	}
-
+	
 }
