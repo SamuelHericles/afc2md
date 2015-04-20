f2032dc382fb86d73721a6ab594ea3f73aabe7a1
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-04-20 15:12:47 -0300

    Refatorando ConversorDeExercise

--- src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeExercise.java
+++ src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeExercise.java
@@ -4,10 +4,12 @@ public class ConversorDeExercise implements ConversorDeSintaxe {
 
 	private static final String REGEX_ABERTURA_EXERCISE = "(?i)\\[EXERCISE\\]";
 	private static final String REGEX_FECHAMENTO_EXERCISE = "(?i)\\[/EXERCISE\\]";
+	private static final String REPLACEMENT_ABERTURA_EXERCISE = "";
+	private static final String REPLACEMENT_FECHAMENTO_EXERCISE = "";
 
 	@Override
 	public String converte(String sintaxe) {
-		return sintaxe.replaceAll(REGEX_ABERTURA_EXERCISE, "").replaceAll(REGEX_FECHAMENTO_EXERCISE, "");
+		return sintaxe.replaceAll(REGEX_ABERTURA_EXERCISE, REPLACEMENT_ABERTURA_EXERCISE).replaceAll(REGEX_FECHAMENTO_EXERCISE, REPLACEMENT_FECHAMENTO_EXERCISE);
 	}
 
 }
