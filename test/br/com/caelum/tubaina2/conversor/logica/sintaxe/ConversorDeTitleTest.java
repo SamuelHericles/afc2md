cb7198997c0b05f2db45a3fd1de73e717d29841f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-04-20 15:06:24 -0300

    Mais testes para ConversorDeTitle

--- test/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeTitleTest.java
+++ test/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeTitleTest.java
@@ -6,26 +6,26 @@ import org.junit.Test;
 
 public class ConversorDeTitleTest {
 
-	private ConversorDeSintaxe convesor;
+	private ConversorDeSintaxe conversor;
 
 	@Before
 	public void setUp() {
-		convesor = new ConversorDeTitle();
+		conversor = new ConversorDeTitle();
 	}
 
 	@Test
 	public void emUmaLinha() {
-		Assert.assertEquals("### Recebendo dados e adicionando comportamentos", convesor.converte("[title Recebendo dados e adicionando comportamentos]"));
+		Assert.assertEquals("### Recebendo dados e adicionando comportamentos", conversor.converte("[title Recebendo dados e adicionando comportamentos]"));
 	}
 
 	@Test
 	public void maiusculo() {
-		Assert.assertEquals("### Wizard para notas fiscais", convesor.converte("[TITLE Wizard para notas fiscais]"));
+		Assert.assertEquals("### Wizard para notas fiscais", conversor.converte("[TITLE Wizard para notas fiscais]"));
 	}
 
 	@Test
 	public void emMaisDeUmaLinha() {
-		String convertido = convesor.converte("[title Onde devo abrir e fechar minhas conexões\n"
+		String convertido = conversor.converte("[title Onde devo abrir e fechar minhas conexões\n"
 												+ " e transações com o banco de dados?]");
 		String esperado = "### Onde devo abrir e fechar minhas conexões\n"
 							+ " e transações com o banco de dados?";
@@ -38,10 +38,27 @@ public class ConversorDeTitleTest {
 		String outra = "Já utilizamos o CDI para realizar a injeção do %%EntityManager%%.";
 		String paragrafo = "Uma solução com CDI\n\n";
 		
-		String convertido = convesor.converte("[title "+ uma+ "]\n\n"+paragrafo+"[title "+outra+"]\n");
+		String convertido = conversor.converte("[title "+ uma+ "]\n\n"+paragrafo+"[title "+outra+"]\n");
 		String esperado = "### "+ uma+ "\n\n"+paragrafo+"### "+outra+"\n";
 		
 		Assert.assertEquals(esperado, convertido);
 	}
+	
+	@Test
+	public void comEspacosNaFrente(){
+		String convertido = conversor.converte("[title                Trabalhando em paralelo com branches]");
+		String esperado = "### Trabalhando em paralelo com branches";
+		
+		Assert.assertEquals(esperado, convertido);
+	}
+
+	@Test
+	public void comQuebrasDeLinhaTabsEEspacosNaFrente(){
+		String convertido = conversor.converte("[title\n   \t\t    \n         Protocolo HTTP/HTTPS]");
+		String esperado = "### Protocolo HTTP/HTTPS";
+		
+		Assert.assertEquals(esperado, convertido);
+	}
+
 	
 }
