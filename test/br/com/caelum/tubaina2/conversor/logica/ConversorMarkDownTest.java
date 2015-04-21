7581d54df6c679475dffcb638eae532d51f376ec
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-04-21 12:00:10 -0300

    Criando testes que verificam arquivos

--- test/br/com/caelum/tubaina2/conversor/logica/ConversorMarkDownTest.java
+++ test/br/com/caelum/tubaina2/conversor/logica/ConversorMarkDownTest.java
@@ -1,6 +1,8 @@
 package br.com.caelum.tubaina2.conversor.logica;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.Assert;
@@ -50,6 +52,28 @@ public class ConversorMarkDownTest {
 		
 		String titulo = md.titulo();
 		Assert.assertEquals("AJAX com JSF 2", titulo);
+	}
+
+	@Test
+	public void notesComCodeEInlineCode() throws IOException, URISyntaxException {
+		AFC afc = criaAfcAPartirDoArquivo("/exemplos/afc/notes_com_code_e_inline_code.afc");
+		
+		MarkDown md = conversor.converte(afc);
+		String conteudoMD = md.conteudo();
+		
+		String conteudoEsperadoMD = obtemMdEsperadoDoArquivo("/exemplos/md/notes_com_code_e_inline_code.md");
+		Assert.assertEquals(conteudoEsperadoMD, conteudoMD);
+	}
+
+	public AFC criaAfcAPartirDoArquivo(String nomeAFC) throws URISyntaxException, IOException {
+		Path pathAFC = Diretorio.getResourceAsPath(nomeAFC);
+		String conteudoAFC = Diretorio.getPathContents(pathAFC);
+		return new AFC(pathAFC, conteudoAFC);
+	}
+
+	public String obtemMdEsperadoDoArquivo(String nomeMD) throws URISyntaxException, IOException {
+		Path pathMD = Diretorio.getResourceAsPath(nomeMD);
+		return Diretorio.getPathContents(pathMD);
 	}
 
 }
---
+++ test/exemplos/md/notes_com_code_e_inline_code.md
@@ -0,0 +1,13 @@
+
+_"O caminho do inferno está pavimentado de boas intenções."  -- Marx_
+
+## Exercícios opcionais: Composite-componentes
+
+Abra o arquivo **campoTexto.xhtml** e nele coloque o namespace padrão dos
+componentes JSF (`xmlns:h="http://java.sun.com/jsf/html"` e
+`xmlns:f="http://java.sun.com/jsf/core"`).
+
+<!--
+Da para fazer o componente ter filhos e incluir esses dentro do implementation
+com `<composite:insertChildren/>`
+-->
