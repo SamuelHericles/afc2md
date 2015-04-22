6cfcc575fb9713a9a47d36232493d925d343a8f8
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-04-22 17:24:54 -0300

    ajustes na regex do conversor de box

--- src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeBox.java
+++ src/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeBox.java
@@ -2,7 +2,7 @@ package br.com.caelum.tubaina2.conversor.logica.sintaxe;
 
 public class ConversorDeBox implements ConversorDeSintaxe {
 
-	private static final String REGEX_ABERTURA_BOX = "\\[box (.*)\\]";
+	private static final String REGEX_ABERTURA_BOX = "(?is)\\[box\\s*(.*?)\\]";
 	private static final String REGEX_FECHAMENTO_BOX = "\\[/box\\]";
 	private static final String REPLACEMENT_ABERTURA_BOX = "> **$1**";
 	private static final String REPLACEMENT_FECHAMENTO_BOX = "";
