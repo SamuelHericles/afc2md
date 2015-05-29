15d678e9f8cdb83ef14d5dcadb307b008d52d630
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-05-29 15:16:43 -0300

    Removendo espaço antes do @todo

--- src/main/java/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeTodo.java
+++ src/main/java/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeTodo.java
@@ -3,7 +3,7 @@ package br.com.caelum.tubaina2.conversor.logica.sintaxe;
 public class ConversorDeTodo implements ConversorDeSintaxe {
 
 	private static final String REGEX_TODO = "(?is)\\[TODO\\s+(.*?)\\]";
-	private static final String REPLACEMENT_TODO = "<!-- @todo $1 -->";
+	private static final String REPLACEMENT_TODO = "<!--@todo $1 -->";
 
 	@Override
 	public String converte(String sintaxe) {
--- src/test/java/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeTodoTest.java
+++ src/test/java/br/com/caelum/tubaina2/conversor/logica/sintaxe/ConversorDeTodoTest.java
@@ -15,12 +15,12 @@ public class ConversorDeTodoTest {
 
 	@Test
 	public void emUmaLinha() {
-		Assert.assertEquals("<!-- @todo exercicios -->", conversor.converte("[todo exercicios]"));
+		Assert.assertEquals("<!--@todo exercicios -->", conversor.converte("[todo exercicios]"));
 	}
 
 	@Test
 	public void maiusculo() {
-		Assert.assertEquals("<!-- @todo é imprescindível o desenho para isso -->", conversor.converte("[TODO é imprescindível o desenho para isso]"));
+		Assert.assertEquals("<!--@todo é imprescindível o desenho para isso -->", conversor.converte("[TODO é imprescindível o desenho para isso]"));
 	}
 
 	@Test
@@ -28,7 +28,7 @@ public class ConversorDeTodoTest {
 		String convertido = conversor.converte("[todo precisa mudar esse screenshot\n"
 												+ " e colocar dados que\n"
 												+ " pareçam de verdade]");
-		String esperado = "<!-- @todo precisa mudar esse screenshot\n"
+		String esperado = "<!--@todo precisa mudar esse screenshot\n"
 											+ " e colocar dados que\n"
 											+ " pareçam de verdade -->";
 		Assert.assertEquals(esperado, convertido);
@@ -41,7 +41,7 @@ public class ConversorDeTodoTest {
 		String paragrafo = "Imagine que nossa %%NotaFiscal%% necessite de muitos dados além de CNPJ e data.\n\n";
 		
 		String convertido = conversor.converte("[TODO "+ uma+ "]\n\n"+paragrafo+"[TODO "+outra+"]\n");
-		String esperado = "<!-- @todo "+ uma+ " -->\n\n"+paragrafo+"<!-- @todo "+outra+" -->\n";
+		String esperado = "<!--@todo "+ uma+ " -->\n\n"+paragrafo+"<!--@todo "+outra+" -->\n";
 		
 		Assert.assertEquals(esperado, convertido);
 	}
@@ -49,7 +49,7 @@ public class ConversorDeTodoTest {
 	@Test
 	public void comEspacosNaFrente(){
 		String convertido = conversor.converte("[TODO          nesse paragrafo ha a confusão clássica de IoC e DI; arrumar isso]");
-		String esperado = "<!-- @todo nesse paragrafo ha a confusão clássica de IoC e DI; arrumar isso -->";
+		String esperado = "<!--@todo nesse paragrafo ha a confusão clássica de IoC e DI; arrumar isso -->";
 		
 		Assert.assertEquals(esperado, convertido);
 	}
@@ -57,7 +57,7 @@ public class ConversorDeTodoTest {
 	@Test
 	public void comQuebrasDeLinhaTabsEEspacosNaFrente(){
 		String convertido = conversor.converte("[TODO\n   \t\t    \n         jsf2: flash]");
-		String esperado = "<!-- @todo jsf2: flash -->";
+		String esperado = "<!--@todo jsf2: flash -->";
 		
 		Assert.assertEquals(esperado, convertido);
 	}
